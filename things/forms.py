da41f723b086814d6f3b07a2e1825d02b0e2332c
Sharujan Sriramasothy <sharujan.sriramasothy@kcl.ac.uk>
2023-10-12 15:31:31 +0100

    Add Thing Form

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,38 @@
 """Forms of the project."""
+from django import forms
+from .models import Thing
+from django.core.validators import RegexValidator
 
 # Create your forms here.
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {
+            'description' : forms.Textarea(), 
+            'quantity' : forms.NumberInput(),
+            }
+        validators = {
+            'name' : [RegexValidator(
+                regex=r'^[A-Za-z ]{1,35}$',
+                message='Name cannot be blank and must be less than 35 characters',
+            )],
+            'quantity' : [RegexValidator(
+                regex=r'^(?:[0-9]|[1-4][0-9]|50)$',
+                message='Quantity must be between 0 and 50.',
+            )],
+        }
+        
+    # quantity = forms.IntegerField(
+    #     widget = forms.NumberInput(),
+    #     validators=[
+    #         RegexValidator(
+    #             regex=r'^(?:[0-9]|[1-4][0-9]|50)$',
+    #             message='New Password must contain an uppercase character, ' 
+    #                     'a lowercase character and a number.',
+    #         ),
+    #     ],
+    # )
+
+    
+
--- things/tests.py
+++ things/tests.py
@@ -1,3 +1,80 @@
+from django import forms
 from django.test import TestCase
+from .forms import ThingForm
 
 # Create your tests here.
+
+class ThingFormTestCase(TestCase):
+
+    def setUp(self):
+        self.form_input = {
+            'name' : 'Jane Doe',
+            'description' : 'Hi, my name is Jane Doe',
+            'quantity' : '1',
+        }
+
+    def test_valid_thing_form(self):
+        form = ThingForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_form_has_necessary_fields(self):
+        form = ThingForm()
+        self.assertIn('name', form.fields)
+        self.assertIn('description', form.fields)
+        description_field = form.fields['description'].widget
+        self.assertTrue(isinstance(description_field, forms.Textarea))
+        self.assertIn('quantity', form.fields)
+        quantity_field = form.fields['quantity'].widget
+        self.assertTrue(isinstance(quantity_field, forms.NumberInput))
+
+    # Test for valid inputs
+
+    def test_form_uses_model_validation(self):
+        self.form_input['name'] = 'James'
+        form = ThingForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_name_cannot_be_blank(self):
+        self.form_input['name'] = ''
+        form = ThingForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
+    
+    def test_description_cannot_be_more_than_120_characters_long(self):
+        self.form_input['description'] = 'x' * 121
+        form = ThingForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
+
+    def test_description_can_be_120_characters_long(self):
+        self.form_input['description'] = 'x' * 120
+        form = ThingForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_description_cannot_be_more_than_120_characters_long(self):
+        self.form_input['description'] = 'x' * 121
+        form = ThingForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
+
+    def test_quantity_cannot_be_blank(self):
+        self.form_input['quantity'] = ''
+        form = ThingForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
+
+    def test_quantity_cannot_be_negative(self):
+        self.form_input['quantity'] = '-1'
+        form = ThingForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
+
+    def test_quantity_can_be_0(self):
+        self.form_input['quantity'] = '0'
+        form = ThingForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_description_cannot_be_more_than_51(self):
+        self.form_input['quantity'] = '51'
+        form = ThingForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
+
+    def test_description_can_be_50(self):
+        self.form_input['quantity'] = '50'
+        form = ThingForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
